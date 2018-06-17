9f8546063b14e8d9ed10f85c189fd474fb0bbc10
Ken <kchau@microsoft.com>
2018-06-16 23:22:12 -0700

    filter data.action

--- FabricWebhook/index.ts
+++ FabricWebhook/index.ts
@@ -6,22 +6,24 @@ const authHandler = vsts.getPersonalAccessTokenHandler(process.env!.VSTS_PAT!);
 const connection = new vsts.WebApi(collectionUrl, authHandler);  
 
 export = async function (context, data) {
-    const project = 'UI Fabric';
+    if (data.action == 'opened' || data.action == 'synchronize') {
+        const project = 'UI Fabric';
 
-    const id = data.number;
-    const ref = `refs/pull/${id}/merge`;
+        const id = data.number;
+        const ref = `refs/pull/${id}/merge`;
 
-    const buildApi = await connection.getBuildApi();
-    const defs: bi.DefinitionReference[] = await buildApi.getDefinitions(project);
+        const buildApi = await connection.getBuildApi();
+        const defs: bi.DefinitionReference[] = await buildApi.getDefinitions(project);
 
-    const def = defs.find(def => def.name == 'UI Fabric - PR Deploy');
-    
-    const build = await buildApi.queueBuild(<bi.Build>{
-      definition: def!,
-      sourceBranch: ref,
-    }, project);
+        const def = defs.find(def => def.name == 'UI Fabric - PR Deploy');
+        
+        const build = await buildApi.queueBuild(<bi.Build>{
+        definition: def!,
+        sourceBranch: ref,
+        }, project);
 
-    
-    context.res = { buildId: build.id };
+        
+        context.res = { buildId: build.id };
+    }
     context.done();
 };
