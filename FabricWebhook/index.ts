6032f787b1468ede16af0576fa94ee65934fca2f
Ken <kchau@microsoft.com>
2018-06-16 23:05:20 -0700

    test #1

---
+++ FabricWebhook/index.ts
@@ -0,0 +1,27 @@
+import * as vsts from 'vso-node-api';
+import * as bi from 'vso-node-api/interfaces/BuildInterfaces';
+
+const collectionUrl = "https://uifabric.visualstudio.com/defaultcollection";
+const authHandler = vsts.getPersonalAccessTokenHandler(process.env!.VSTS_PAT!); 
+const connection = new vsts.WebApi(collectionUrl, authHandler);  
+
+export = async function (context, data) {
+    const project = 'UI Fabric';
+
+    const id = data.number;
+    const ref = `refs/pull/${id}/merge`;
+
+    const buildApi = await connection.getBuildApi();
+    const defs: bi.DefinitionReference[] = await buildApi.getDefinitions(project);
+
+    const def = defs.find(def => def.name == 'UI Fabric - PR Deploy');
+    
+    const build = await buildApi.queueBuild(<bi.Build>{
+      definition: def!,
+      sourceBranch: ref,
+    }, project);
+
+    
+    context.res = { buildId: build.id };
+    context.done();
+};
